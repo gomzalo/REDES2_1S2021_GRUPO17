e185704ee19ccc9987e232c2e8e12737b5a3668f
A <201318652@ingenieria.usac.edu.gt>
2021-03-26 02:16:15 -0600

    Contenedores creados, faltan endpoints

--- Practica4/Server/db/mysql/setup.sql
+++ Practica4/Server/db/mysql/setup.sql
@@ -1,44 +1,30 @@
 /***CREATING ALL TABLES*/
-CREATE TABLE EMPLOYEE (
-  EmployeeId   INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  FirstName    VARCHAR(40)                    NULL,
-  LastName     VARCHAR(40)                    NULL,
-  Phone        VARCHAR(20)                    NULL,
-  Address      VARCHAR(100)                   NULL,
-  City         VARCHAR(30)                    NULL,
-  FullName     VARCHAR(100)                   NULL,
-  Email        VARCHAR(100)                   NULL,
-  Pin          VARCHAR(100)                   NULL,
-  CreationDate DATETIME                       NULL,
-  EmployeeCode VARCHAR(10) UNIQUE             NOT NULL
-)
-  ENGINE = INNODB;
+CREATE TABLE Reporte(
+        Carnet INT PRIMARY KEY NOT NULL,
+        Nombre VARCHAR(50) NULL,
+        Curso VARCHAR(50) NULL,
+        Fecha VARCHAR(100) NULL,
+        Cuerpo VARCHAR(120) NULL
+);
+
+--   ENGINE = INNODB;
 
 /* INSERT DATA */
-INSERT INTO EMPLOYEE (FirstName, LastName, Phone, Address, City, FullName, Email, Pin, CreationDate, EmployeeCode)
-VALUES ('Marta', 'Wayne', 945214775, 'Av. Alfonso Ugarte', 'Lima', 'Marta Wayne', 'sysadmin@gmail.com',
-        1, '2011-12-18 13:17:17', 'SYSADMIN');
-INSERT INTO EMPLOYEE (FirstName, LastName, Phone, Address, City, FullName, Email, Pin, CreationDate, EmployeeCode)
-VALUES ('Luis', 'Hernandez', 945214775, 'Av. Alfonso Ugarte', 'Lima', 'Luis Hernandez', 'sysadmin@gmail.com',
-        1, '2011-12-18 13:17:17', 'SYSADMIN');
-VALUES ('Marta', 'Kent', 945214775, 'Av. Alfonso Ugarte', 'Lima', 'SYSADMIN SYSADMIN', 'sysadmin@gmail.com',
-        1, '2011-12-18 13:17:17', 'SYSADMIN');
-INSERT INTO EMPLOYEE (FirstName, LastName, Phone, Address, City, FullName, Email, Pin, CreationDate, EmployeeCode)
-VALUES ('Marcos', 'Guzman', 945214775, 'Av. Alfonso Ugarte', 'Lima', 'SYSADMIN SYSADMIN', 'sysadmin@gmail.com',
-        1, '2011-12-18 13:17:17', 'SYSADMIN');
-INSERT INTO EMPLOYEE (FirstName, LastName, Phone, Address, City, FullName, Email, Pin, CreationDate, EmployeeCode)
-VALUES ('Lorena', 'Lopez', 945214775, 'Av. Alfonso Ugarte', 'Lima', 'SYSADMIN SYSADMIN', 'sysadmin@gmail.com',
-        1, '2011-12-18 13:17:17', 'SYSADMIN');
-INSERT INTO EMPLOYEE (FirstName, LastName, Phone, Address, City, FullName, Email, Pin, CreationDate, EmployeeCode)
+INSERT INTO Reporte (Carnet, Nombre, Curso, Cuerpo)
+VALUES (201588645, 'Tepokun', 'Redes 3', 'Ay no');
+INSERT INTO Reporte (Carnet, Nombre, Curso, Cuerpo)
+VALUES (201358845, 'Niu', 'Redes 3', 'Ay no');
+INSERT INTO Reporte (Carnet, Nombre, Curso, Cuerpo)
+VALUES (201555556, 'Macaco', 'Redes 3', 'Ay no');
 
-DROP PROCEDURE IF EXISTS sp_GetEmployee;
-DELIMITER //
-CREATE PROCEDURE sp_GetEmployee()
-  BEGIN
-    SELECT * FROM EMPLOYEE;
-  END //
-DELIMITER ;
-/**Drop StoreProcedure**/
-CALL sp_GetEmployee();
+-- DROP PROCEDURE IF EXISTS sp_GetEstudiante;
+-- DELIMITER //
+-- CREATE PROCEDURE sp_GetEstudiante()
+--   BEGIN
+--     SELECT * FROM DATOS_ESTUDIANTE;
+--   END //
+-- DELIMITER ;
+-- /**Drop StoreProcedure**/
+-- CALL sp_GetEstudiante();
 
-
+Commit;
